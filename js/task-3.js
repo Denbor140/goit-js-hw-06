8ed7d690cf9d96ca15f3b49fa996ce3d8f5e69c2
MAK <denbor140@gmail.com>
2025-10-18 18:55:56 +0300

    fix

--- js/task-3.js
+++ js/task-3.js
@@ -8,13 +8,13 @@ class StringBuilder {
     return this.#value;
   }
   padEnd(str) {
-    this.#value = this.#value.padEnd(3, str);
+    this.#value = this.#value + str;
   }
   padStart(str) {
-    this.#value = this.#value.padStart(2, str);
+    this.#value = str + this.#value;
   }
   padBoth(str) {
-    this.#value = this.#value.padStart(4, str).padEnd(5, str);
+    this.#value = str + this.#value + str;
   }
 }
 
